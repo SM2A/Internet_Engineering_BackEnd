ed08808b7f1dbc03301c6ea64a1b34236d9ab6bd
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 20:52:32 +0330

    implement provider controller

--- src/main/java/ir/ut/ece/ie/controller/provider/ProviderController.java
+++ src/main/java/ir/ut/ece/ie/controller/provider/ProviderController.java
@@ -1,7 +1,17 @@
 package ir.ut.ece.ie.controller.provider;
 
+import ir.ut.ece.ie.domain.provider.Provider;
 import ir.ut.ece.ie.service.provider.ProviderService;
+import ir.ut.ece.ie.service.provider.ProviderServiceImpl;
 
 public class ProviderController {
     private ProviderService providerService;
+
+    public ProviderController() {
+        this.providerService = new ProviderServiceImpl();
+    }
+
+    public Provider addProvider(Provider provider) {
+        return providerService.addProvider(provider);
+    }
 }
