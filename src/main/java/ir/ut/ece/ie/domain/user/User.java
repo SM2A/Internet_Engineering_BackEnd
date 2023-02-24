63f425bcd8a147c86a8c2c38ef72ba98ca11f391
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 20:22:47 +0330

    change birthDate type

--- src/main/java/ir/ut/ece/ie/domain/user/User.java
+++ src/main/java/ir/ut/ece/ie/domain/user/User.java
@@ -1,12 +1,10 @@
 package ir.ut.ece.ie.domain.user;
 
-import java.time.LocalDate;
-
 public class User {
     private String username;
     private String password;
     private String email;
-    private LocalDate birthDate;
+    private String birthDate;
     private String address;
     private Long credit;
 }
