bed65f1de66ebe6302e8f87eac6d56b321eb2f9a
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 18:12:43 +0330

    add user and provider controller

---
+++ src/main/java/ir/ut/ece/ie/controller/provider/ProviderController.java
@@ -0,0 +1,7 @@
+package ir.ut.ece.ie.controller.provider;
+
+import ir.ut.ece.ie.service.provider.ProviderService;
+
+public class ProviderController {
+    private ProviderService providerService;
+}
---
+++ src/main/java/ir/ut/ece/ie/controller/user/UserController.java
@@ -0,0 +1,7 @@
+package ir.ut.ece.ie.controller.user;
+
+import ir.ut.ece.ie.service.user.UserService;
+
+public class UserController {
+    private UserService userService;
+}
