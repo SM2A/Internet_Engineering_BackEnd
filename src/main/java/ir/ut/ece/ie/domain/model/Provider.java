4760a8c5d4c156a1131793acff9d505186d41ac4
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 17:38:07 +0330

    resolve conflicts

--- src/main/java/ir/ut/ece/ie/domain/model/Provider.java
+++
@@ -1,9 +0,0 @@
-package ir.ut.ece.ie.domain.model;
-
-import java.time.LocalDate;
-
-public class Provider {
-    private Integer id;
-    private String name;
-    private LocalDate registryDate;
-}
