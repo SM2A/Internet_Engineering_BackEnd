ffca8ce8dc352e530332821a794601d2fcfe7b7a
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 17:21:05 +0330

    refactor in domain models

---
+++ src/main/java/ir/ut/ece/ie/domain/shopping/BuyList.java
@@ -0,0 +1,8 @@
+package ir.ut.ece.ie.domain.shopping;
+
+import java.util.List;
+
+public class BuyList {
+    private String username;
+    private List<Long> commodityIds;
+}
--- src/main/java/ir/ut/ece/ie/domain/user/BuyItem.java
+++
@@ -1,6 +0,0 @@
-package ir.ut.ece.ie.domain.user;
-
-public class BuyItem {
-    private String username;
-    private Long commodityId;
-}
