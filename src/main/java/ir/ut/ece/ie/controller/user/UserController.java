d8cf265bc33bf7100e74f5f646f40db7ebfb9356
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 20:51:57 +0330

    implement user controller

--- src/main/java/ir/ut/ece/ie/controller/user/UserController.java
+++ src/main/java/ir/ut/ece/ie/controller/user/UserController.java
@@ -1,7 +1,15 @@
 package ir.ut.ece.ie.controller.user;
 
+import ir.ut.ece.ie.domain.user.User;
 import ir.ut.ece.ie.service.user.UserService;
+import ir.ut.ece.ie.service.user.UserServiceImpl;
 
 public class UserController {
     private UserService userService;
+    public UserController() {
+        this.userService = new UserServiceImpl();
+    }
+    public User addUser(User user) {
+        return userService.addUser(user);
+    }
 }
