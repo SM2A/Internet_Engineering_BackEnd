4b62f0b2bae6d43670bb556dccc0d23a2188602a
morteza-nouri <morteza1noori@gmail.com>
2023-02-24 20:31:48 +0330

    change registryDate type to string

--- src/main/java/ir/ut/ece/ie/domain/provider/Provider.java
+++ src/main/java/ir/ut/ece/ie/domain/provider/Provider.java
@@ -1,9 +1,7 @@
 package ir.ut.ece.ie.domain.provider;
 
-import java.time.LocalDate;
-
 public class Provider {
     private Integer id;
     private String name;
-    private LocalDate registryDate;
+    private String registryDate;
 }
