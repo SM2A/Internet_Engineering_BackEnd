dac4ee7e192f80d7850f8a77ec70dca344279380
morteza-nouri <morteza1noori@gmail.com>
2023-02-25 18:39:13 +0330

    do some refactor

--- src/main/java/ir/ut/ece/ie/service/user/UserServiceImpl.java
+++ src/main/java/ir/ut/ece/ie/service/user/UserServiceImpl.java
@@ -3,15 +3,14 @@ package ir.ut.ece.ie.service.user;
 import ir.ut.ece.ie.domain.user.User;
 import ir.ut.ece.ie.exception.OnlineShopException;
 import ir.ut.ece.ie.repository.user.UserRepository;
-import ir.ut.ece.ie.repository.user.UserRepositoryImpl;
 
 import java.util.regex.Pattern;
 
 public class UserServiceImpl implements UserService {
     private UserRepository repository;
 
-    public UserServiceImpl() {
-        this.repository = new UserRepositoryImpl();
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
     }
 
     @Override
