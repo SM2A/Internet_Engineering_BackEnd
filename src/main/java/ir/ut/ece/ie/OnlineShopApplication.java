89037d689a875e02582a686b0fbaef5411156403
morteza-nouri <morteza1noori@gmail.com>
2023-03-13 01:07:38 +0330

    add javalin routes

--- src/main/java/ir/ut/ece/ie/OnlineShopApplication.java
+++ src/main/java/ir/ut/ece/ie/OnlineShopApplication.java
@@ -1,20 +1,24 @@
 package ir.ut.ece.ie;
 
 import io.javalin.Javalin;
+import ir.ut.ece.ie.controller.commodity.CommodityController;
 import ir.ut.ece.ie.util.DataInitializer;
+import ir.ut.ece.ie.util.Factory;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
 
 public class OnlineShopApplication {
     public static void main(String[] args) throws IOException, InterruptedException {
         DataInitializer.loadData("http://5.253.25.110:5000");
-        Javalin app = Javalin.create().start(8000);
-        app.get("/", ctx -> ctx.html("Hello world"));
+        CommodityController commodityController = Factory.getCommodityController();
 
-        Map<String, Object> model = new HashMap<>();
-        model.put("name", "morteza");
-        app.get("/render", ctx -> ctx.render("home.pebble", model));
+        Javalin app = Javalin.create().start(8000);
+        app.get("/", ctx -> ctx.render("templates/base.peb"));
+        app.get("/commodities", ctx ->
+                ctx.render(
+                        "templates/commodities.peb",
+                        Collections.singletonMap("commodities", commodityController.getCommodities())));
+        
     }
 }
