d56725ec609b7999d0505b143c55cc583e830d1a
morteza-nouri <morteza1noori@gmail.com>
2023-03-31 00:47:06 +0430

    add commodities path and jsp

--- src/main/java/ir/ut/ece/ie/util/Path.java
+++ src/main/java/ir/ut/ece/ie/util/Path.java
@@ -5,11 +5,13 @@ public class Path {
         public static final String HOME = "/";
         public static final String LOGIN = "/login";
         public static final String LOGOUT = "/logout";
+        public static final String COMMODITIES = "/commodities";
     }
 
     public static class JSP {
         public static final String HOME = "/jsp/home.jsp";
         public static final String LOGIN = "/jsp/login.jsp";
         public static final String ERROR = "/jsp/error.jsp";
+        public static final String COMMODITIES = "/jsp/commodities.jsp";
     }
 }
--- src/main/webapp/jsp/home.jsp
+++ src/main/webapp/jsp/home.jsp
@@ -1,4 +1,5 @@
 <%@ page import="ir.ut.ece.ie.util.Factory" %>
+<%@ page import="ir.ut.ece.ie.util.Path" %>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <html lang="en">
 <head>
@@ -9,7 +10,7 @@
 <ul>
     <li id="username">username: <%= Factory.getUserController().getLoggedInUser().getUsername() %></li>
     <li>
-        <a href="/commodities">Commodities</a>
+        <a href=<%=Path.Web.COMMODITIES%>>Commodities</a>
     </li>
     <li>
         <a href="/buyList">Buy List</a>
