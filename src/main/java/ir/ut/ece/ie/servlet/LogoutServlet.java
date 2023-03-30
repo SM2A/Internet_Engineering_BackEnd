1a8ef8801c5c9c1f6299f41ee5cc742450448095
morteza-nouri <morteza1noori@gmail.com>
2023-03-30 19:19:25 +0430

    implement logout servlet

---
+++ src/main/java/ir/ut/ece/ie/servlet/LogoutServlet.java
@@ -0,0 +1,28 @@
+package ir.ut.ece.ie.servlet;
+
+import ir.ut.ece.ie.controller.user.UserController;
+import ir.ut.ece.ie.util.Factory;
+import ir.ut.ece.ie.util.Path;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = Path.Web.LOGOUT)
+public class LogoutServlet extends HttpServlet {
+    private final UserController userController = Factory.getUserController();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (userController.getLoggedInUser() != null) {
+            userController.logout();
+            resp.sendRedirect(Path.Web.HOME);
+        } else {
+            req.setAttribute("errorMsg", "There is currently logged in user");
+            req.getRequestDispatcher(Path.JSP.ERROR).forward(req, resp);
+        }
+    }
+}
