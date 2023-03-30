ebe1dcc198bf23bfa113dcebaf3deeaf602fa995
morteza-nouri <morteza1noori@gmail.com>
2023-03-30 19:11:22 +0430

    implement user login and logout

--- src/main/java/ir/ut/ece/ie/controller/user/UserController.java
+++ src/main/java/ir/ut/ece/ie/controller/user/UserController.java
@@ -6,9 +6,24 @@ import ir.ut.ece.ie.service.user.UserService;
 
 public class UserController {
     private final UserService userService;
+    private User loggedInUser;
 
     public UserController(UserService service) {
         this.userService = service;
+        this.loggedInUser = null;
+    }
+
+    public User login(User user) {
+        loggedInUser = user;
+        return loggedInUser;
+    }
+
+    public void logout() {
+        loggedInUser = null;
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
     }
 
     public User addUser(User user) {
