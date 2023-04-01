ebeebbf9f6e40e235310bdc9a624f7cc09622e70
morteza-nouri <morteza1noori@gmail.com>
2023-04-01 19:00:49 +0430

    implement calculate buyList price method

--- src/main/java/ir/ut/ece/ie/service/buylist/BuyListService.java
+++ src/main/java/ir/ut/ece/ie/service/buylist/BuyListService.java
@@ -3,12 +3,14 @@ package ir.ut.ece.ie.service.buylist;
 import ir.ut.ece.ie.domain.buylist.BuyList;
 import ir.ut.ece.ie.domain.commodity.Commodity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BuyListService {
     Commodity addToBuyList(String username, Long commodityId);
 
     Optional<BuyList> getBuyList(String username);
+    Long calculateBuyListPrice(List<Commodity> commodities);
 
     void removeFromBuyList(String username, Long commodityId);
 }
