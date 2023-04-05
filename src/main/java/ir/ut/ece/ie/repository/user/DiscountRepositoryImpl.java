21227cebc3af859dfd4b5221c1ae770584755155
morteza-nouri <morteza1noori@gmail.com>
2023-04-05 22:42:10 +0430

    add discount repository

---
+++ src/main/java/ir/ut/ece/ie/repository/user/DiscountRepositoryImpl.java
@@ -0,0 +1,28 @@
+package ir.ut.ece.ie.repository.user;
+
+import ir.ut.ece.ie.domain.user.Discount;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DiscountRepositoryImpl implements DiscountRepository {
+    private final Map<String, Discount> discounts = new HashMap<>();
+
+    @Override
+    public Discount save(Discount discount) {
+        discounts.put(discount.getCode(), discount);
+        return discount;
+    }
+
+    @Override
+    public Iterable<Discount> saveAll(Iterable<Discount> discounts) {
+        discounts.forEach(discount -> this.discounts.put(discount.getCode(), discount));
+        return discounts;
+    }
+
+    @Override
+    public Optional<Discount> findByCode(String code) {
+        return Optional.ofNullable(discounts.get(code));
+    }
+}
