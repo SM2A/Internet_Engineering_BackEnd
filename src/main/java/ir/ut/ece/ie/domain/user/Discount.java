faaa64ccc47ebf172eecc6a24d40171a21aa72d2
morteza-nouri <morteza1noori@gmail.com>
2023-04-05 22:43:46 +0430

    fix non-matching field name in deserialization

--- src/main/java/ir/ut/ece/ie/domain/user/Discount.java
+++ src/main/java/ir/ut/ece/ie/domain/user/Discount.java
@@ -1,11 +1,14 @@
 package ir.ut.ece.ie.domain.user;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Discount {
+    @SerializedName(value = "code", alternate = "discountCode")
     private String code;
+    @SerializedName(value = "amount", alternate = "discount")
     private Integer amount;
 }
