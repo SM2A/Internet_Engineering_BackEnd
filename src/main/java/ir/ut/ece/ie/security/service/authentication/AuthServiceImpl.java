effbddf96482350c4f3a6070fc70ab689e6a803e
morteza-nouri <morteza1noori@gmail.com>
2023-05-30 23:27:40 +0430

    complete signup

--- src/main/java/ir/ut/ece/ie/security/service/authentication/AuthServiceImpl.java
+++ src/main/java/ir/ut/ece/ie/security/service/authentication/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import ir.ut.ece.ie.api.model.authentication.AuthenticationResponse;
 import ir.ut.ece.ie.api.model.authentication.LoginRequest;
 import ir.ut.ece.ie.api.model.authentication.SignupRequest;
 import ir.ut.ece.ie.domain.user.User;
+import ir.ut.ece.ie.exception.OnlineShopException;
 import ir.ut.ece.ie.repository.user.UserRepository;
 import ir.ut.ece.ie.security.jwt.JWTProvider;
 import lombok.RequiredArgsConstructor;
@@ -31,10 +32,17 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public void signup(SignupRequest request) {
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new OnlineShopException("username already used.");
+        } else if (userRepository.existsByEmail(request.getEmail())) {
+            throw new OnlineShopException("email already used.");
+        }
         User user = new User();
         user.setUsername(request.getUsername());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setEmail(request.getEmail());
+        user.setBirthDate(request.getBirthDate());
+        user.setAddress(request.getAddress());
         userRepository.save(user);
     }
 }
